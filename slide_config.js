e51b0be2549163d89e4dcfca8b81831a0c3dde8b
Arnout Roemers <github@functionalbytes.nl>
2016-02-26 17:02:13 +0100

    Rename to index.org and add generated files

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'Containium', 
+     subtitle: 'Application Server as a Library', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/containium-s.png', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Arnout Roemers & Danny Wilson', 
+     company: '', 
+     gplus: '', 
+     twitter: '@containium', 
+     www: 'http://containium.org', 
+     github: 'http://github.com/containium', 
+   }]
+};
